ae9b280c831396fef51e2a417b5de9d1cdb6c7ed
Mathilde <mathilde.ziboura@abc-culture.fr>
2019-03-26 17:33:26 +0100

    change tests

--- cdiscountapi/tests/test_user_info.py
+++ cdiscountapi/tests/test_user_info.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# 
 # -*- coding: utf-8 -*-
 #
 # Copyright © 2019 Alexandria
@@ -14,22 +12,14 @@ def test_get_seller_info():
     """Test get_user_info  with a mock."""
 
     expected_response = open("cdiscountapi/tests/samples/get_seller_info", "r", encoding='utf8').read()
-    api = CdiscountApi(credentials={
-        'wsdl': 'https://wsvc.cdiscount.com/MarketplaceAPIService.svc?wsdl',
-        'token': 'thetoken',
-        'login': 'thebestlogin'
-    })
+    api = CdiscountApi(token='BestToken')
     response = api.get_seller_info()
 
     assert response == expected_response
 
 
 def test_get_user_info_type_none():
-    api = CdiscountApi({
-        'wsdl': 'https://wsvc.cdiscount.com/MarketplaceAPIService.svc?wsdl',
-        'token': 'thetoken',
-        'login': 'thebestlogin'
-    })
+    api = CdiscountApi(token='BestToken')
     with pytest.raises(CdiscountApiTypeError):
         api.get_seller_info(user_id=None)
 
@@ -38,10 +28,6 @@ def test_get_user_info_bad_response():
     with Mocker() as m:
         api_response = open("cdiscountapi/tests/samples/seller_info_bad_request", "r", encoding='utf8').read()
         m.get('https://wsvc.cdiscount.com/MarketplaceAPIService.svc?wsdl', text=api_response)
-        api = CdiscountApi(credentials={
-            'wsdl': 'https://wsvc.cdiscount.com/MarketplaceAPIService.svc?wsdl',
-            'token': 'thetoken',
-            'login': 'thebestlogin'
-        })
+        api = CdiscountApi(token='BestToken')
         result = api.get_seller_info()
         assert result is None
