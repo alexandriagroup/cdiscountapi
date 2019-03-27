3563cf2531f45a7adef22e0803b5f2c2aa639e05
Taurus Olson <taurusolson@gmail.com>
2019-03-27 12:07:48 +0100

    Automate the retrieval of the token
    * TODO: Catch the exception when the token has expired and retrieve the
      token again

--- cdiscountapi/cdiscountapi.py
+++ cdiscountapi/cdiscountapi.py
@@ -3,14 +3,20 @@
 # Copyright © 2019 Alexandria
 
 from zeep import Client, helpers
+import requests
+import lxml
 
 
 class CdiscountApi(object):
     """A class ton manage the interaction with the CdiscountMarketplace API"""
-    def __init__(self, token):
+    def __init__(self, login, password):
         self.wsdl = 'https://wsvc.cdiscount.com/MarketplaceAPIService.svc?wsdl'
-        # self.token = token
+        self.auth_url = ('https://sts.cdiscount.com/users/httpIssue.svc/'
+                         '?realm=https://wsvc.cdiscount.com/MarketplaceAPIService.svc')
+        self.login = login
+        self.password = password
         self.client = Client(self.wsdl)
+        self.token = self.get_token()
         self.header = {
             'Context': {
                 'SiteID': 100
@@ -21,11 +27,15 @@ class CdiscountApi(object):
             'Security': {
                 'IssuerID': None,
                 'SessionID': None,
-                'TokenId': token,
+                'TokenId': self.token,
                 'UserName': '',
             },
             'Version': 1.0,
         }
+
+    def get_token(self):
+        response = requests.get(self.auth_url, auth=(self.login, self.password))
+        return lxml.etree.XML(response.text).text
 
     def get_seller_info(self):
         """
@@ -42,7 +52,6 @@ class CdiscountApi(object):
         :return: a dict with the data of the user
         :rtype: dict
         """
-
         response = self.client.service.GetSellerIndicators(headerMessage=self.header)
         return helpers.serialize_object(response, dict)
 
