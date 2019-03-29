eed7b2e4252ff8ff90d719b941166a35bb971ebb
Taurus Olson <taurusolson@gmail.com>
2019-03-29 12:25:49 +0100

    Use vcrpy to mock the tests

--- cdiscountapi/tests/test_user_info.py
+++ cdiscountapi/tests/test_user_info.py
@@ -2,32 +2,80 @@
 #
 # Copyright © 2019 Alexandria
 
-from requests_mock import Mocker
+import os
+import glob
+import re
+import requests
 import pytest
-from ..cdiscountapi import CdiscountApi
+from ..cdiscountapi import Connection
 from ..exceptions import CdiscountApiTypeError
 
 
+def read_fixture(filename):
+    with open('cdiscountapi/tests/samples/{}'.format(filename),
+              'r', encoding='utf8') as f:
+        return f.read()
+
+
+@pytest.mark.vcr()
+def test_initialization():
+    """
+    The token and the header_message should be updated at initialization
+    """
+    api = Connection(os.getenv('CDISCOUNT_API_LOGIN'),
+                     os.getenv('CDISCOUNT_API_PASSWORD'))
+    assert re.match(r'[a-z0-9]{32}', api.token) is not None
+    assert api.header == {
+        'Context': {
+            'SiteID': 100
+        },
+        'Localization': {
+            'Country': 'Fr',
+        },
+        'Security': {
+            'IssuerID': None,
+            'SessionID': None,
+            'TokenId': api.token,
+            'UserName': '',
+        },
+        'Version': 1.0,
+    }
+
+
+@pytest.mark.vcr()
+def test_token():
+    """
+    get_token should return the token
+    """
+    api = Connection(os.getenv('CDISCOUNT_API_LOGIN'),
+                     os.getenv('CDISCOUNT_API_PASSWORD'))
+    assert re.match(r'[a-z0-9]{32}', api.token) is not None
+
+
+@pytest.mark.vcr()
 def test_get_seller_info():
-    """Test get_user_info  with a mock."""
-
-    expected_response = open("cdiscountapi/tests/samples/get_seller_info", "r", encoding='utf8').read()
-    api = CdiscountApi(token='BestToken')
-    response = api.get_seller_info()
-
-    assert response == expected_response
-
-
-def test_get_user_info_type_none():
-    api = CdiscountApi(token='BestToken')
-    with pytest.raises(CdiscountApiTypeError):
-        api.get_seller_info(user_id=None)
+    """
+    get_seller_info should return the information about the seller
+    """
+    api = Connection(os.getenv('CDISCOUNT_API_LOGIN'),
+                     os.getenv('CDISCOUNT_API_PASSWORD'))
+    response = api.seller.get_seller_info()
+    assert response['ErrorList'] is None
+    assert response['ErrorMessage'] is None
+    assert response['OperationSuccess'] is True
+    assert 'Seller' in response
 
 
-def test_get_user_info_bad_response():
-    with Mocker() as m:
-        api_response = open("cdiscountapi/tests/samples/seller_info_bad_request", "r", encoding='utf8').read()
-        m.get('https://wsvc.cdiscount.com/MarketplaceAPIService.svc?wsdl', text=api_response)
-        api = CdiscountApi(token='BestToken')
-        result = api.get_seller_info()
-        assert result is None
+# def test_get_user_info_type_none():
+    # api = Connection(token='BestToken')
+    # with pytest.raises(CdiscountApiTypeError):
+    #     api.get_seller_info(user_id=None)
+
+
+# # def test_get_user_info_bad_response():
+# #     with Mocker() as m:
+# #         api_response = read_fixture("seller_info_bad_request.xml")
+# #         m.get(WSDL_URL, text=api_response)
+# #         api = Connection(token='BestToken')
+# #         result = api.get_seller_info()
+# #         assert result is None
