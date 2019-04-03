8c1f2135f1d52102514b5451790a7669919bfa30
Taurus Olson <taurusolson@gmail.com>
2019-04-03 11:05:40 +0200

    Rename test_user_info -> test_seller

--- cdiscountapi/tests/test_seller.py
+++ cdiscountapi/tests/test_seller.py
@@ -11,12 +11,6 @@ from ..cdiscountapi import Connection
 from ..exceptions import CdiscountApiTypeError
 
 
-def read_fixture(filename):
-    with open('cdiscountapi/tests/samples/{}'.format(filename),
-              'r', encoding='utf8') as f:
-        return f.read()
-
-
 @pytest.mark.vcr()
 def test_initialization():
     """
@@ -66,16 +60,16 @@ def test_get_seller_info():
     assert 'Seller' in response
 
 
-# def test_get_user_info_type_none():
-    # api = Connection(token='BestToken')
-    # with pytest.raises(CdiscountApiTypeError):
-    #     api.get_seller_info(user_id=None)
-
-
-# # def test_get_user_info_bad_response():
-# #     with Mocker() as m:
-# #         api_response = read_fixture("seller_info_bad_request.xml")
-# #         m.get(WSDL_URL, text=api_response)
-# #         api = Connection(token='BestToken')
-# #         result = api.get_seller_info()
-# #         assert result is None
+@pytest.mark.vcr()
+def test_get_seller_indicators():
+    """
+    get_seller_indicators should return the performance indicators of the
+    seller
+    """
+    api = Connection(os.getenv('CDISCOUNT_API_LOGIN'),
+                     os.getenv('CDISCOUNT_API_PASSWORD'))
+    response = api.seller.get_seller_indicators()
+    assert response['ErrorList'] is None
+    assert response['ErrorMessage'] is None
+    assert response['OperationSuccess'] is True
+    assert 'SellerIndicators' in response
