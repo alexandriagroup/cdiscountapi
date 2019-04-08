fc1c4e534e0a599747c49f7df744b2ed9751149b
Mathilde <mathilde.ziboura@abc-culture.fr>
2019-04-08 16:49:22 +0200

    use assert_response_succeeded

--- cdiscountapi/tests/test_products.py
+++ cdiscountapi/tests/test_products.py
@@ -7,6 +7,7 @@ import os
 from unittest import skip
 
 import pytest
+from . import assert_response_succeeded
 
 from ..cdiscountapi import Connection
 
@@ -17,36 +18,28 @@ api = Connection(os.getenv('CDISCOUNT_API_LOGIN'),
 @pytest.mark.vcr()
 def test_get_allowed_category_tree():
     response = api.products.get_allowed_category_tree()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'CategoryTree' in response.keys()
 
 
 @pytest.mark.vcr()
 def test_get_all_allowed_category_tree():
     response = api.products.get_all_allowed_category_tree()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'CategoryTree' in response.keys()
 
 
 @pytest.mark.vcr()
 def test_get_product_list():
     response = api.products.get_product_list()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'ProductList' in response.keys()
 
 
 @pytest.mark.vcr()
 def test_get_model_list():
     response = api.products.get_model_list()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'ModelList' in response.keys()
 
 
@@ -54,18 +47,14 @@ def test_get_model_list():
 @pytest.mark.vcr()
 def test_get_all_model_list():
     response = api.products.get_model_list()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'ModelList' in response.keys()
 
 
 @pytest.mark.vcr()
 def test_get_brand_list():
     response = api.products.get_brand_list()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'BrandList' in response.keys()
 
 
@@ -76,34 +65,26 @@ def test_submit_product_package():
         products_dict = json.loads(f.read())
     url = 'toto.html/'
     response = api.products.submit_product_package(products_dict, url)
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'PackageId' in response.keys()
 
 
 @pytest.mark.vcr()
 def test_get_product_package_submission_result():
     response = api.products.get_product_package_submission_result()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'PackageId' in response.keys()
 
 
 @pytest.mark.vcr()
 def test_get_product_package_product_matching_file_data():
     response = api.products.get_product_package_product_matching_file_data()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'ProductMatchingList' in response.keys()
 
 
 @pytest.mark.vcr()
 def test_get_product_list_by_identifier():
     response = api.products.get_product_list_by_identifier()
-    assert response['ErrorList'] is None
-    assert response['ErrorMessage'] is None
-    assert response['OperationSuccess'] is True
+    assert_response_succeeded(response)
     assert 'ProductListByIdentifier' in response.keys()
