274214c30503908f1403683312580337676dfae4
Taurus Olson <taurusolson@gmail.com>
2019-04-12 10:11:38 +0200

    Finish Discussions

--- cdiscountapi/cdiscountapi.py
+++ cdiscountapi/cdiscountapi.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 #
 # Copyright © 2019 Alexandria
 
@@ -779,6 +779,28 @@ class Discussions(object):
         return helpers.serialize_object(response, dict)
 
     def get_offer_question_list(self, **offer_question_filter):
+        """
+        Return the list of questions about offers with the specified criteria
+
+        :param offer_question_filter: The keywords for the filter
+        `offerQuestionFilter`:
+
+        - BeginCreationDate
+        - BeginModificationDate
+        - EndCreationDate
+        - EndModificationDate
+        - StatusList
+        - ProductEANList
+        - ProductSellerReferenceList
+
+        Example:
+        >>> response = api.get_offer_question_list(
+            StatusList={'DiscussionStateFilter': 'Open'}
+            )
+
+        :returns: An OfferQuestionListMessage dictionary.
+
+        """
         offer_question_filter = self.api.factory.OfferQuestionFilter(
             **offer_question_filter
         )
@@ -788,11 +810,59 @@ class Discussions(object):
         )
         return helpers.serialize_object(response, dict)
 
-    def get_order_question_list(self):
-        pass
+    def get_order_question_list(self, **order_question_filter):
+        """
+        Return the list of questions about orders with the specified criteria
 
-    def close_discussion_list(self):
-        pass
+        :param order_question_filter: The keywords for the filter
+        `orderQuestionFilter`:
+
+        - BeginCreationDate
+        - BeginModificationDate
+        - EndCreationDate
+        - EndModificationDate
+        - StatusList
+        - ProductEANList
+        - ProductSellerReferenceList
+
+        Example:
+        >>> response = api.get_order_question_list(
+            StatusList={'DiscussionStateFilter': 'Open'}
+            )
+
+        :returns: An OrderQuestionListMessage dictionary.
+
+        """
+        order_question_filter = self.api.factory.OrderQuestionFilter(
+            **order_question_filter
+        )
+        response = self.api.client.service.GetOrderQuestionList(
+            headerMessage=self.api.header,
+            orderQuestionFilter=order_question_filter
+        )
+        return helpers.serialize_object(response, dict)
+
+    def close_discussion_list(self, discussion_ids):
+        """
+        Close a discussion list
+
+        :type discussion_ids: list
+        :param discussion_ids: The list of discussion_ids to close
+
+        Example:
+        >>> response = api.discussions.close_discussion_list([31, 4, 159])
+        """
+        arrays_factory = self.api.client.type_factory(
+            'http://schemas.microsoft.com/2003/10/Serialization/Arrays'
+        )
+        close_discussion_request = self.api.factory.CloseDiscussionRequest(
+            DiscussionIds=arrays_factory.ArrayOflong(discussion_ids)
+        )
+        response = self.api.client.service.CloseDiscussionList(
+            headerMessage=self.api.header,
+            closeDiscussionRequest=close_discussion_request
+        )
+        return helpers.serialize_object(response, dict)
 
 
 class WebMail(object):
