46d26eb74641449ae491e5fd3f306ff21977e00a
Taurus Olson <taurusolson@gmail.com>
2019-04-12 16:49:47 +0200

    Implement WebMail

--- cdiscountapi/cdiscountapi.py
+++ cdiscountapi/cdiscountapi.py
@@ -940,20 +940,51 @@ class Discussions(object):
 
 
 class WebMail(object):
+    """
+    The WebMail API allows the seller to retrieve encrypted email address to
+    contact a customer
+    """
 
     def __init__(self, api):
         self.api = api
 
-    def generate_mail_discussion_guid(self):
+    def generate_discussion_mail_guid(self, scopus_id=None):
         """
         Obtain an encrypted mail address.
 
         This operation allows getting an encrypted mail address to contact a
         customer about an order.
-        """
 
-    def get_discussion_mail_list(self):
-        pass
+        Usage:
+        >>> response = api.generate_discussion_mail_guid(scopus_id)
+        """
+        response = self.api.client.service.GenerateDiscussionMailGuid(
+            headerMessage=self.api.header,
+            request={'ScopusId': scopus_id}
+        )
+        return helpers.serialize_object(response, dict)
+
+    def get_discussion_mail_list(self, discussion_ids):
+        """
+        Obtain an encrypted mail address about a discussion.
+
+        This operation allows getting an encrypted mail address to contact a
+        customer about a discussion (claim, retraction, questions).
+
+        Usage:
+        >>> response = api.webmail.generate_discussion_mail_guid(discussion_ids)
+        """
+        arrays_factory = self.api.client.type_factory(
+            'http://schemas.microsoft.com/2003/10/Serialization/Arrays'
+        )
+        request = self.api.factory.GetDiscussionMailListRequest(
+            DiscussionIds=arrays_factory.ArrayOflong(discussion_ids)
+        )
+        response = self.api.client.service.GetDiscussionMailList(
+            headerMessage=self.api.header,
+            request=request
+        )
+        return helpers.serialize_object(response, dict)
 
 
 class Connection(object):
