1ad1cdc3f9b190cd8649ddd6e7003301aa3675f2
Mathilde <mathilde.ziboura@abc-culture.fr>
2019-04-15 16:07:40 +0200

    Finish fulfillment section

--- cdiscountapi/tests/test_fulfillments.py
+++ cdiscountapi/tests/test_fulfillments.py
@@ -44,5 +44,70 @@ def test_get_fulfillment_delivery_document():
     assert 'ReportList' in response.keys()
 
 
+@skip('not authorized to access')
+@pytest.mark.vcr()
+def test_get_fulfillment_supply_order():
+    response = api.fulfillment.get_fulfillment_supply_order(PageNumber=1)
+    assert_response_succeeded(response)
+    assert 'SupplyOrderLineList' in response.keys()
 
 
+@skip('submit not ready')
+@pytest.mark.vcr()
+def test_submit_fulfillment_activation():
+    response = api.fulfillment.submit_fulfillment_activation()
+    assert_response_succeeded(response)
+    assert 'DepositId' in response.keys()
+
+
+@skip('not authorized to access')
+@pytest.mark.vcr()
+def test_get_fulfillment_activation_report_list():
+    response = api.fulfillment.get_fulfillment_activation_report_list()
+    assert_response_succeeded(response)
+    assert 'FulfilmentActivationReportList' in response.keys()
+
+
+@skip('not authorized to access')
+@pytest.mark.vcr()
+def test_get_fulfillment_order_list_to_supply():
+    response = api.fulfillment.get_fulfillment_order_list_to_supply()
+    assert_response_succeeded(response)
+    assert 'FulfilmentActivationReportList' in response.keys()
+
+
+@skip('submit not ready')
+@pytest.mark.vcr()
+def test_submit_offer_state_action():
+    response = api.fulfillment.submit_offer_state_action(
+        Action='Unpublish',
+        SellerProductId=11504
+    )
+    assert_response_succeeded(response)
+
+
+@skip('not authorized to access')
+@pytest.mark.vcr()
+def test_create_external_order():
+    response = api.fulfillment.create_external_order()
+    assert_response_succeeded(response)
+
+
+@pytest.mark.vcr()
+def test_get_external_order_status():
+    response = api.fulfillment.get_external_order_status(
+        Corporation='FNAC',
+        CustomerOrderNumber='YLA_test_APIFBC_FODETP_20160414_02'
+    )
+    assert_response_succeeded(response)
+    assert 'Status' in response.keys()
+
+
+@pytest.mark.vcr()
+def test_get_product_stock_list():
+    response = api.fulfillment.get_product_stock_list(
+        BarCodeList=['3515450012475'],
+        FulfilmentReferencement=None
+    )
+    assert_response_succeeded(response)
+    assert 'ProductStockList' in response.keys()
