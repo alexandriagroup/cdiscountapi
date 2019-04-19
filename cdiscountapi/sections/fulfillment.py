ccfb8f70987e487aa4375b454c12a102a7bd1182
Taurus Olson <taurusolson@gmail.com>
2019-04-19 15:59:09 +0200

    Fix get_fulfillment_supply_order_report_list and get_fulfillment_supply_order in Fulfillment

--- cdiscountapi/sections/fulfillment.py
+++ cdiscountapi/sections/fulfillment.py
@@ -46,14 +46,36 @@ class Fulfillment(object):
     def get_fulfillment_supply_order_report_list(self, **request):
         """
         To search supply order reports.
-        :param request:
-            'BeginCreationDate': date,
-            'DepositIdList': int list,
-            'EndCreationDate': date,
-            'PageNumber': int,
-            'PageSize': int
+
+        :param request: The keywords used to filter the supply order reports:
+
+        - PageSize (int) [mandatory]
+        - BeginCreationDate, EndCreationDate (date)
+        - PageNumber (int)
+        - DepositIdList (list of ints)
+
+        Examples::
+
+            response = api.fulfillment.get_fulfillment_supply_order_report_list(
+                PageSize=10,
+                BeginCreationDate=datetime.datetime(2019, 1, 1),
+                EndCreationDate=datetime.datetime(2019, 1, 2),
+            )
+
+            response = api.fulfillment.get_fulfillment_supply_order_report_list(
+                PageSize=10, DepositIdList=[1, 2, 3]
+            )
+
         :return:supply order reports
         """
+        if 'DepositIdList' in request:
+            arrays_factory = self.api.client.type_factory(
+                'http://schemas.microsoft.com/2003/10/Serialization/Arrays'
+            )
+            request.update(DepositIdList=arrays_factory.ArrayOfint(
+                request['DepositIdList'])
+            )
+
         supply_order_report_request = self.api.factory.SupplyOrderReportRequest(**request)
         response = self.api.client.service.GetFulfilmentSupplyOrderReportList(
             headerMessage=self.api.header,
@@ -75,26 +97,59 @@ class Fulfillment(object):
 
     def get_fulfillment_supply_order(self, **request):
         """
-        :param request:
-            'BeginModificationDate': date,
-            'EndModificationDate': date,
-            'PageNumber': int,
-            'PageSize': int,
-            'SupplyOrderNumberList': string list
+        :param request: The keywords used to filter the supply order reports:
+
+        - PageSize (int) [mandatory]
+        - BeginCreationDate, EndCreationDate (date)
+        - PageNumber (int)
+        - DepositIdList (list of ints)
+        - SupplyOrderNumberList (list of strings)
+
+        Examples::
+
+            response = api.fulfillment.get_fulfillment_supply_order_report_list(
+                PageSize=10,
+                BeginCreationDate=datetime.datetime(2019, 1, 1),
+                EndCreationDate=datetime.datetime(2019, 1, 2),
+            )
+
+            response = api.fulfillment.get_fulfillment_supply_order_report_list(
+                PageSize=10, SupplyOrderNumberList=['X', 'Y', 'Z']
+            )
+
         :return: supply orders
         """
+        if 'SupplyOrderNumberList' in request:
+            arrays_factory = self.api.client.type_factory(
+                'http://schemas.microsoft.com/2003/10/Serialization/Arrays'
+            )
+            request.update(SupplyOrderNumberList=arrays_factory.ArrayOfstring(
+                request['SupplyOrderNumberList'])
+            )
 
+        if 'DepositIdList' in request:
+            arrays_factory = self.api.client.type_factory(
+                'http://schemas.microsoft.com/2003/10/Serialization/Arrays'
+            )
+            request.update(DepositIdList=arrays_factory.ArrayOfint(
+                request['DepositIdList'])
+            )
+
+        supply_order_report_request = self.api.factory.SupplyOrderRequest(**request)
         response = self.api.client.service.GetFulfilmentSupplyOrder(
             headerMessage=self.api.header,
-            request=request
+            request=supply_order_report_request
         )
         return serialize_object(response, dict)
 
     def submit_fulfillment_activation(self, request):
         """
         To ask for products activation (or deactivation)
+
         :param request:
-            'ProductList': ProductActivationData list
+
+        'ProductList': ProductActivationData list
+
         :return: deposit id
         """
         response = self.api.client.service.SubmitFulfilmentActivation(
@@ -135,6 +190,7 @@ class Fulfillment(object):
     def submit_offer_state_action(self, **request):
         """
         To set an offer online or offline
+
         :param request:
             'Action': 'Publish' or 'Unpublish'
             'SellerProductId': str
@@ -150,22 +206,28 @@ class Fulfillment(object):
     def create_external_order(self, order):
         """
         create an order from another marketplace.
-        :param order: dict as {
-                    'Comments': str,
-                    'Corporation': str (ex:FNAC),
-                    'Customer': customer info dict,
-                    'CustomerOrderNumber': str,
-                    'OrderDate': date,
-                    'OrderLineList': {
-                        'ExternalOrderLine': {
-                            'ProductEan': str,
-                            'ProductReference': str,
-                            'Quantity': int
-                        }
-                    },
-                    'ShippingMode': str,
 
-        :return:
+        :param order: A dictionary with the structure:
+
+        .. code-block:: python
+
+            {
+                'Comments': str,
+                'Corporation': str (ex:FNAC),
+                'Customer': customer info dict,
+                'CustomerOrderNumber': str,
+                'OrderDate': date,
+                'OrderLineList': {
+                    'ExternalOrderLine': {
+                        'ProductEan': str,
+                        'ProductReference': str,
+                        'Quantity': int
+                    }
+                },
+                'ShippingMode': str,
+            }
+
+        :return: The response
         """
         response = self.api.client.service.CreateExternalOrder(
             headerMessage=self.api.header,
