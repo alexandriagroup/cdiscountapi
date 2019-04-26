c16d657cf95e80da184198d0f626768e6ffed72f
Taurus Olson <taurusolson@gmail.com>
2019-04-26 16:47:42 +0200

    Test auto_refresh_token

--- cdiscountapi/tests/test_helpers.py
+++ cdiscountapi/tests/test_helpers.py
@@ -1,13 +1,21 @@
+# Python imports
 import os
+import re
 import json
-import pytest
-from shutil import rmtree
+import datetime
 from tempfile import gettempdir
+from shutil import rmtree
+
+# Third-party imports
+import pytest
+
+# Project imports
+from cdiscountapi.cdiscountapi import Connection
 from cdiscountapi.helpers import (
     check_element, generate_offer_package, generate_product_package,
     XmlGenerator
 )
-import datetime
+from . import assert_response_succeeded
 
 
 @pytest.mark.vcr()
@@ -116,9 +124,8 @@ def test_check_element_with_invalid_element(api):
 
 
 # XmlGenerator
-@pytest.mark.vcr()
-def test_validate_offer():
-    generator = XmlGenerator()
+@pytest.fixture
+def valid_offer():
     discount_component = {
         'DiscountValue': 5, 'Type': 1,
         'StartDate': datetime.datetime(2019, 4, 15),
@@ -136,7 +143,13 @@ def test_validate_offer():
         'DiscountList': [discount_component],
         'ShippingInformationList': [shipping_info1, shipping_info2]
     }
-    generator.validate_offer(**offer)
+    return offer
+
+
+@pytest.mark.vcr()
+def test_validate_offer(valid_offer):
+    generator = XmlGenerator()
+    generator.validate_offer(**valid_offer)
 
 
 @pytest.mark.vcr()
@@ -162,3 +175,40 @@ def test_validate_offer_with_invalid_key():
 
     # A TypeError should be raised because shipping_info2 has an invalid key
     pytest.raises(TypeError, generator.validate_offer, offer)
+
+
+@pytest.mark.vcr()
+def test_add_offers():
+    raise AssertionError
+
+
+@pytest.mark.vcr()
+def test_auto_refresh_token():
+    # We get rid of the fixture 'api' that has a cache because we want to do a
+    # new request to the server
+    api = Connection(os.getenv('CDISCOUNT_API_LOGIN'),
+                     os.getenv('CDISCOUNT_API_PASSWORD'),
+                     header_message={
+                         'Context': {
+                             'SiteID': 100,
+                             'CatalogID': 1
+                         },
+                         'Localization': {
+                             'Country': 'Fr',
+                         },
+                         'Security': {
+                             'UserName': '',
+                         },
+                         'Version': '1.0',
+                     })
+
+    # We purposely set an invalid token so that a zeep.exceptions.Fault is raised
+    api.header['Security']['TokenId'] = 'invalid_token'
+    api.token = 'invalid_token'
+    # Then we run any operation
+    response = api.seller.get_seller_info()
+    assert_response_succeeded(response)
+
+    # The token should be valid now
+    assert api.token == api.header['Security']['TokenId']
+    assert re.match(r'[a-z0-9]{32}', api.token) is not None
