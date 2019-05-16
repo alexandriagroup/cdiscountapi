ef1f0e3a107fbc0dad4bd99bfd3f81b4155a33ec
Taurus Olson <taurusolson@gmail.com>
2019-05-16 17:30:39 +0200

    Fix test_webmail

--- cdiscountapi/tests/test_webmail.py
+++ cdiscountapi/tests/test_webmail.py
@@ -2,11 +2,10 @@ import pytest
 from . import assert_response_succeeded, assert_response_failed, CDISCOUNT_WITHOUT_DATA
 
 
-# TODO Finish the test
 @pytest.mark.skipif(CDISCOUNT_WITHOUT_DATA, reason='Waiting for orders')
 @pytest.mark.vcr()
 def test_generate_mail_discussion_guid(api):
-    response = api.webmail.generate_discussion_mail_guid('1904241640CLE8Z')
+    response = api.webmail.generate_discussion_mail_guid('1904291413BT4WY')
     assert_response_succeeded(response)
     assert response['MailGuid'] is not None
 
@@ -24,7 +23,7 @@ def test_get_discussion_mail_list(api):
     response = api.webmail.get_discussion_mail_list([113163877])
     assert_response_succeeded(response)
     assert response['DiscussionMailList']['DiscussionMail'][0]['DiscussionId'] == 113163877
-    assert response['DiscussionMailList']['DiscussionMail'][0]['OperationStatus'] == 'Success'
+    assert response['DiscussionMailList']['DiscussionMail'][0]['OperationStatus'] == 'DiscussionClosed'
     assert response['DiscussionMailList']['DiscussionMail'][0]['MailAddress'] is not None
 
 
