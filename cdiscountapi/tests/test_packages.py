2ef1618362f9566ce86337dc36d0b998489d1f28
Taurus Olson <taurusolson@gmail.com>
2019-05-17 16:12:03 +0200

    Fix test_packages

--- cdiscountapi/tests/test_packages.py
+++ cdiscountapi/tests/test_packages.py
@@ -12,9 +12,7 @@ def test_OfferPackage(valid_offer_for_package):
     package = OfferPackage({'OfferCollection': [valid_offer_for_package]})
     assert len(package.data) == 1
     offer_for_package = package.data[0]
-    assert offer_for_package['Price'] == valid_offer_for_package['Price']
-    assert offer_for_package['SellerProductId'] == valid_offer_for_package['SellerProductId']
-    assert offer_for_package['DiscountList'] == valid_offer_for_package['DiscountList']
+    assert offer_for_package == valid_offer_for_package
 
 
 @pytest.mark.vcr()
