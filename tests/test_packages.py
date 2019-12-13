be975199f50ea3f3076ad76066c0989df90ee2a5
Taurus Olson <taurusolson@gmail.com>
2019-12-13 10:58:05 +0100

    Test OfferPackage with "StockAndPrice" and "Full"

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -8,11 +8,28 @@ from cdiscountapi.exceptions import ValidationError
 
 # OfferPackage
 @pytest.mark.vcr()
-def test_OfferPackage(valid_offer_for_package):
-    package = OfferPackage({"OfferCollection": [{"Offer": valid_offer_for_package}]})
+def test_OfferPackage_with_PackageType_Full(valid_offer_for_package):
+    # The required keys must be ProductEan and SellerProductId
+    package = OfferPackage(
+        {"OfferCollection": [{"Offer": valid_offer_for_package}], "PackageType": "Full"}
+    )
     assert len(package.data) == 1
     offer_for_package = package.data[0]
     assert offer_for_package == valid_offer_for_package
+
+
+@pytest.mark.vcr()
+def test_OfferPackage_with_PackageType_StockAndPrice(valid_offer_for_package):
+    # The required keys must be ProductEan and SellerProductId
+    offer = {
+        "ProductEan": "978-1593274351", "SellerProductId": 'MY_SKU1', "Price": 10,
+    }
+    package = OfferPackage(
+        {"OfferCollection": [{"Offer": offer}], "PackageType": "StockAndPrice"}
+    )
+    assert len(package.data) == 1
+    offer_for_package = package.data[0]
+    assert offer_for_package == offer
 
 
 @pytest.mark.vcr()
@@ -21,6 +38,19 @@ def test_OfferPackage_with_invalid_key(valid_offer_for_package):
     invalid_offer_for_package["InvalidKey"] = "Unknown"
     # A ValidationError should be raised because InvalidKey is neither required
     # or optional
+    pytest.raises(
+        ValidationError, OfferPackage, {
+            "OfferCollection": [{"Offer": invalid_offer_for_package}]
+        }
+    )
+
+
+@pytest.mark.parametrize("key", ["ProductEan", "SellerProductId"])
+@pytest.mark.vcr()
+def test_OfferPackage_with_missing_key(valid_offer_for_package, key):
+    invalid_offer_for_package = valid_offer_for_package
+    del invalid_offer_for_package[key]
+    # A ValidationError should be raised because ProductEan is required
     pytest.raises(
         ValidationError, OfferPackage, {
             "OfferCollection": [{"Offer": invalid_offer_for_package}]
